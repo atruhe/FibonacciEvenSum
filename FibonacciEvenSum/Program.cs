1d2ec4aea0d6cd5e7e7c0c4936d752f2f89f70c7
atruhe <atruhe@hotmail.com>
2020-09-28 15:08:32 -0500

    Solved Fibonacci

--- FibonacciEvenSum/Program.cs
+++ FibonacciEvenSum/Program.cs
@@ -10,6 +10,25 @@ namespace FibonacciEvenSum
     {
         static void Main(string[] args)
         {
+            //  Each new term in the Fibonacci sequence is generated
+            //  by adding the previous two terms.By starting with
+            //  1 and 2, the first 10 terms will be:
+            //  1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+            //By considering the terms in the Fibonacci sequence 
+            //whose values do not exceed four million, 
+            //find the sum of the even-valued terms.
+
+            int num1 = 0, num2 = 1, sum;
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                sum = num1 + num2;
+                Console.WriteLine(sum);
+                num1 = num2;
+                num2 = sum;
+            }
+
         }
     }
 }
